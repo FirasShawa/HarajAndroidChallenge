7fd51bc9e7d4588558abbdefd83dfc912f044df1
FirasAlShawa <firas.alshawa@gmail.com>
2021-07-25 17:28:14 +0300

    Share feature

--- app/src/main/java/com/example/harajtask/ui/main/PostDetails.kt
+++ app/src/main/java/com/example/harajtask/ui/main/PostDetails.kt
@@ -1,15 +1,16 @@
 package com.example.harajtask.ui.main
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.harajtask.R
 import com.example.harajtask.model.post.Post
 import com.example.harajtask.utils.Constants
 import kotlinx.android.synthetic.main.activity_post_details.*
 
-class PostDetails : AppCompatActivity() {
+class PostDetails : AppCompatActivity(), View.OnClickListener {
     lateinit var post:Post
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +26,19 @@ class PostDetails : AppCompatActivity() {
         tv_post_details_date.text = Constants.ConvertTimeStampToDate(post.date)
         tv_post_details_username.text = post.username
         tv_post_details_body.text = post.body
+
+        ib_post_details_share.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.ib_post_details_share -> {
+                val shareIntent = Intent(Intent.ACTION_SEND)
+                shareIntent.type = "text/plain"
+                val share_value = "Hey Check out this post on Haraj app:\n\n${post.title}\n\n${post.body}\n\n${post.username}"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, share_value)
+                startActivity(Intent.createChooser(shareIntent, "Share via"))
+            }
+        }
     }
 }
